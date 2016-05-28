24210e0166ba481d02cbdfdd04b7ab0408b0edaf
unknown <>
2016-05-28 21:34:02 +0800

    add update

--- net.java.amateras.umleditor/src/net/java/amateras/uml/classdiagram/model/GeneralizationModel.java
+++ net.java.amateras.umleditor/src/net/java/amateras/uml/classdiagram/model/GeneralizationModel.java
@@ -1,7 +1,10 @@
 package net.java.amateras.uml.classdiagram.model;
 
+import java.util.*;
+
 import org.eclipse.ui.views.properties.ColorPropertyDescriptor;
 import org.eclipse.ui.views.properties.IPropertyDescriptor;
+import org.eclipse.ui.views.properties.PropertyDescriptor;
 import org.eclipse.ui.views.properties.TextPropertyDescriptor;
 
 import net.java.amateras.uml.UMLPlugin;
@@ -10,16 +13,24 @@ import net.java.amateras.uml.model.AbstractUMLConnectionModel;
 public class GeneralizationModel extends AbstractUMLConnectionModel {
 
 	private String transCond="";
+	private List<String> updates = new ArrayList<String>();
 	public static final String P_TRANS_COND = "_transform_condition";
+	public static final String P_UPDATE = "_UPDATE";
+	public GeneralizationModel(){
+		
+		updates.add("abc");
+		updates.add("def");
+	}
 	public IPropertyDescriptor[] getPropertyDescriptors() {
 		IPropertyDescriptor []propertyDescriptors = super.getPropertyDescriptors();
-		int newLength = propertyDescriptors.length+1;
+		int newLength = propertyDescriptors.length+2;
 		IPropertyDescriptor[] newPropertyDescriptors = new IPropertyDescriptor[newLength];
 		System.arraycopy(propertyDescriptors, 0, newPropertyDescriptors, 0, propertyDescriptors.length);
 
-		newPropertyDescriptors[newLength - 1] =
+		newPropertyDescriptors[newLength - 2] =
 			new TextPropertyDescriptor(P_TRANS_COND, "转移条件"/*UMLPlugin.getDefault().getResourceString("property.abstract")*/);
-
+		newPropertyDescriptors[newLength - 1] = 
+				new TextPropertyDescriptor(P_UPDATE, "转移动作"/*UMLPlugin.getDefault().getResourceString("property.attributes")*/);
 		propertyDescriptors = newPropertyDescriptors;
 		return propertyDescriptors;
 	}
@@ -35,6 +46,12 @@ public class GeneralizationModel extends AbstractUMLConnectionModel {
 		if (id.equals(P_TRANS_COND)) {
 			return getTransCond();
 			//return new Boolean(isAbstract());
+		}else if(id.equals(P_UPDATE)){
+			String s = "";
+			Iterator<String> iter = updates.iterator();
+			while(iter.hasNext())
+				s = s+iter.next()+"; ";
+			return s;
 		}
 		return super.getPropertyValue(id);
 	}
@@ -42,14 +59,29 @@ public class GeneralizationModel extends AbstractUMLConnectionModel {
 	public boolean isPropertySet(Object id) {
 		if (id.equals(P_TRANS_COND)) {
 			return true;
+		}else if(id.equals(P_UPDATE)){
+			return true;
 		}
+
 		return super.isPropertySet(id);
 	}
 
 	public void setPropertyValue(Object id, Object value) {
 		if (id.equals(P_TRANS_COND)) {
 			setTransCond((String)value);
+		}else if(id.equals(P_UPDATE)){
+			String s = (String)value;
+			String[] ups = s.split(";");
+			updates = new ArrayList();
+			for(int i = 0; i < ups.length; i++){
+				if(ups[i].trim().length()>0)
+					updates.add(ups[i].trim());
+				
+			}
+				
+			
 		}
+	
 		super.setPropertyValue(id, value);
 	}
 }
